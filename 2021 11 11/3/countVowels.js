9d4828a7b435a6034c9a29c42f9ccb1ea3d89da8
Pavel <a3791820@gmail.com>
2021-11-15 10:43:37 +0300

    added replaced resolve for countVowels

--- 2021 11 11/3/countVowels.js
+++ 2021 11 11/3/countVowels.js
@@ -1,11 +1,9 @@
 const countVowelLetters = (str) => {
-    const massOfVowels = str.split("").filter(el => {
-        if (el === "а" || el === "у" || el === "о" || el === "ы" || el === "и" || el === "э" || el === "я" || el === "ю" || el === "ё" || el === "е") {
-            return el
-        }
-    })
-    
-    return massOfVowels.length
+    const vowels = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"]
+
+    const result = str.split("").filter(el => vowels.indexOf(el) >= 0)
+
+    return result.length
 }
 
 console.log(countVowelLetters("Пришла зима, запахло"))
