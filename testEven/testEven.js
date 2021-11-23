cea25e3c80b1fb4ad9eebc880f87303f10f287c4
Pavel <a3791820@gmail.com>
2021-11-23 12:03:11 +0300

    refactoring

--- testEven/testEven.js
+++ testEven/testEven.js
@@ -2,7 +2,6 @@ const userNumbers = prompt("Enter some numbers")
 
 const numbers = [...userNumbers]
 
-// 1.
 numbers.reduce((acc, num, i, arr) => {
     if (!(Number(acc) % 2) && !(Number(num) % 2)) {
         arr.splice(i - 1, 1, `${acc}-`)
