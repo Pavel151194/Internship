311188d475946e339b1f26c1771b922241e8f7df
Pavel <a3791820@gmail.com>
2021-11-23 12:25:47 +0300

    refactoring

--- countVowels/countVowels.js
+++ countVowels/countVowels.js
@@ -1,7 +1,7 @@
 const countVowelLetters = (str) => {
     const vowels = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"]
 
-    return [...str.toLowerCase()].filter(letter => vowels.indexOf(letter) !== -1).length
+    return [...str.toLowerCase()].filter(letter => vowels.includes(letter)).length
 }
 
 console.log(countVowelLetters("Пришла зима, запахло..."))
