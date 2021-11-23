6b319e68a229aba2cc72f79fa0c964a8efa9acad
Pavel <a3791820@gmail.com>
2021-11-23 12:06:15 +0300

    refactoring

--- quadratic/quadratic.js
+++ quadratic/quadratic.js
@@ -1,20 +1,18 @@
 const quadraticEquation = (a, b, c) => {
-    let x1 = null
-    let x2 = null
-    const diskriminant = ( Math.pow(b, 2) ) - (4 * a * c)
+    const diskriminant = Math.pow(b, 2) - (4 * a * c)
 
     if (diskriminant < 0) {
         return 'уравнение не имеет вещественных корней'
     }
 
     if (diskriminant === 0) {
-        x1 = -b / 2 * a
-        return `уравнение имеет один корень x = ${x1}`
+        const x = -b / 2 * a
+        return `уравнение имеет один корень x = ${x}`
     }
 
     if (diskriminant > 0) {
-        x1 = (-b + Math.sqrt(diskriminant)) / 2 * a
-        x2 = (-b - Math.sqrt(diskriminant)) / 2 * a
+        const x1 = (-b + Math.sqrt(diskriminant)) / 2 * a
+        const x2 = (-b - Math.sqrt(diskriminant)) / 2 * a
         return `уравнение имеет корни x1 = ${x1} и x2 = ${x2}`
     }
 }
